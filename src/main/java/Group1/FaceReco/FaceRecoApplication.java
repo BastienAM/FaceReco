62960feabba5c0d853a208313045b535b348fbc6
Arnaud--Mozdzer Bastien <bastien.arnaud--mozdzer@etudiant.univ-rennes1.fr>
2020-05-03 16:32:44 +0200

    Ajout opencv_contrib en librairie interne.

--- src/main/java/Group1/FaceReco/FaceRecoApplication.java
+++ src/main/java/Group1/FaceReco/FaceRecoApplication.java
@@ -1,8 +1,7 @@
 package Group1.FaceReco;
 
 import java.io.File;
-
-import org.opencv.core.Core;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FaceRecoApplication {
 
 	public static void main(String[] args) {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		//System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		//System.loadLibrary("src/main/resources/opencv_contrib/opencv_java420.dll");
+		//System.load("C:/Users/Basti/git/FaceReco/src/main/resources/opencv_contrib/opencv_java420.dll");
+		
+		String absolute = null;
+		
+		try {
+			absolute = new File("./src/main/resources/opencv_contrib/opencv_java420.dll").getCanonicalPath();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.load(absolute);
 		
 		new File("./context").mkdirs();
 		new File("./photo").mkdirs();
