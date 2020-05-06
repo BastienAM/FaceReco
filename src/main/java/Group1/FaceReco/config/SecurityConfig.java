0f162c4711760529adb865559e8cd50f37c28df8
Arnaud--Mozdzer Bastien <bastien.arnaud--mozdzer@etudiant.univ-rennes1.fr>
2020-05-06 16:14:11 +0200

    Désactiver authentification /api/swagger.json

--- src/main/java/Group1/FaceReco/config/SecurityConfig.java
+++ src/main/java/Group1/FaceReco/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api/**").authenticated().and().httpBasic();
+    	http.cors().and().csrf().disable().authorizeRequests().antMatchers("/api/swagger.json").anonymous().and().authorizeRequests().antMatchers("/api/**").authenticated().and().httpBasic();
     }
     
     @Bean
