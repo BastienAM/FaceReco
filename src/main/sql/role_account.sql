012ce6489ac8c4ffc5bb0ee3de10e8bf4bedf065
Arnaud--Mozdzer Bastien <bastien.arnaud--mozdzer@etudiant.univ-rennes1.fr>
2020-05-08 20:58:51 +0200

    changement commentaire SQL.

--- src/main/sql/role_account.sql
+++ src/main/sql/role_account.sql
@@ -3,4 +3,4 @@ INSERT INTO Role_User (Wording) VALUES ('Administrateur');
 UPDATE Role_Right SET allow = TRUE WHERE pk_fk_role_user = 1;
 
 INSERT INTO Account (username, password, fk_role_user) VALUES
-('admin', '$2a$10$d7VCgG17hZgO/y1AW15AoeIf3lQxSIs80lYscKF2SKqqfRyLcEeQG', 1);//mdp : admin
+('admin', '$2a$10$d7VCgG17hZgO/y1AW15AoeIf3lQxSIs80lYscKF2SKqqfRyLcEeQG', 1); -- mdp : admin
