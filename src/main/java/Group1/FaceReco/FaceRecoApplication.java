df500ec1b49ed030549193f94f6f677579f28cfa
Arnaud--Mozdzer Bastien <bastien.arnaud--mozdzer@etudiant.univ-rennes1.fr>
2020-05-08 19:01:25 +0200

    Correction dépendance OpenCV-contrib.

--- src/main/java/Group1/FaceReco/FaceRecoApplication.java
+++ src/main/java/Group1/FaceReco/FaceRecoApplication.java
@@ -1,25 +1,17 @@
 package Group1.FaceReco;
 
 import java.io.File;
-import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FaceRecoApplication {
-
+	
 	public static void main(String[] args) {
 		
 		//Chargement de la librairie native d'openCV_contrib.
-		String absolutePath = null;
-		try {
-			absolutePath = new File("./src/main/resources/opencv_contrib/opencv_java420.dll").getCanonicalPath();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		
-		System.load(absolutePath);
+		System.loadLibrary("opencv_contrib-4.2");
 		
 		//Création des dossiers s'ils existent pas 
 		new File("./context").mkdirs();
